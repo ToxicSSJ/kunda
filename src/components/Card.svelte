a26771b16625f4ffe7f14b4dd5beb0ccdaebc019
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-19 11:52:04 -0500

    added timeline skeleton

---
+++ src/components/Card.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Comments from './Comments.svelte';
+</script>
+
+<div class="Card">
+  <div class="Card-container">
+    <div class="Card-header">
+      <div class="Card-user">
+        <img src="https://cdn.itoxic.me/low_profile.png" alt="Toxic#2381">
+        <h2>
+          Toxic#2381
+          <span>Medellín, Colombia</span>
+        </h2>
+      </div>
+      <div>
+        <i class="fas fa-ellipsis-h" />
+      </div>
+    </div>
+    <div class="Card-photo">
+      <figure>
+        <img src="https://cdn.itoxic.me/low_profile.png" alt="Toxic#2381">
+      </figure>
+    </div>
+    <div class="Card-icons">
+      <div class="Card-icons-first">
+        <i class="fas fa-heart" />
+        <i class="fas fa-share-alt" />
+      </div>
+      <div class="Card-icons-second">
+        <i class="fas fa-bookmark" />
+      </div>
+      <div class="Card-description">
+        <h3>Toxic#2381</h3>
+        <span>Hola!</span>
+      </div>
+      <Comments />
+    </div>
+  </div>
+</div>
---
+++ src/components/Comments.svelte
@@ -0,0 +1,16 @@
+<div class="Comments">
+  <div class="Comments-content">
+    <div class="Comments-users">
+      <h3>Kakaroto</h3>
+      <span>Hola Toxic#2381</span>
+    </div>
+    <div class="Comments-add">
+      <form>
+        <input type="text" 
+        class="Comments-input" 
+        placeholder="Agregar Comentario...">
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  </div>
+</div>
