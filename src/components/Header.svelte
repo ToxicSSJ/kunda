e72eff3b1825aca79398b597a550795b55090556
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-19 12:19:14 -0500

    styles were added

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,66 @@
+<style>
+  .Header {
+    background-color: #ffffff;
+    position: fixed;
+    width: 100%;
+  }
+  .Header-container {
+    grid-template-columns: minmax(auto, 936px);
+    display: grid;
+    justify-content: center;
+    background-color: #ffffff;
+    border-bottom: 1px solid rgba(38, 38, 38, 0.4);
+  }
+  .Header-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5em;
+  }
+  .Header ul {
+    list-style: none;
+  }
+  .Header li {
+    display: inline-block;
+    margin: 0 0 0 1em;
+  }
+  .Header i {
+    font-size: 16px;
+    color: rgba(38, 38, 38, 0.7);
+    cursor: pointer;
+  }
+
+  h1 {
+    padding: 0;
+    margin: 0;
+    font-size: 28px;
+    color: black;
+    font-family: "Aclonica", bold;
+    cursor: pointer;
+    position: relative;
+    background: #fff;
+    mix-blend-mode: multiply;
+    display: inline-block;
+  }
+
+  h1:before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(circle at top left, rgb(127, 206, 127), lime);
+    mix-blend-mode: screen;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+</style>
+
 <div class="Header">
   <div class="Header-container">
     <div class="Header-content">
       <div class="Header-logo">
-        <h1>Kunda</h1>
+        <h1>👽 Kunda</h1>
       </div>
       <div class="Header-nav">
         <ul>
