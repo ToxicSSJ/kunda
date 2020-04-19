20ba5d72b4faf65614c711d4f58ae08f1b7d6770
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-19 11:18:14 -0500

    added sidebar skeleton

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://cdn.itoxic.me/low_profile.png" alt="" />
+    </div>
+    <div class="Profile-info">
+      <h2>Toxic#2381</h2>
+      <span>Abraham M. Lora</span>
+    </div>
+  </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,19 @@
+<div class="Stories">
+  <div class="Stories-container">
+    <div class="Stories-head">
+      <h2>Historias</h2>
+      <span>Ver todas</span>
+    </div>
+    <div class="Stories-items">
+      <div class="Stories-item">
+        <div class="Stories-item-box">
+          <img src="https://cdn.itoxic.me/low_profile.png" alt="">
+        </div>
+        <h2>
+          Toxic#2381
+          <span>10 horas antes</span>
+        </h2>
+      </div>
+    </div>
+  </div>
+</div>
