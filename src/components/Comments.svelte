3c4a7bac84183249888c3eaad3074e23cc5ae3bb
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-23 02:23:23 -0500

    comments integration

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -1,3 +1,22 @@
+<script>
+  
+  export let comments = [];
+
+  function addComment(event) {
+    const msg = event.target.text.value;
+    if (msg.length > 3) {
+      const message = {
+        id: Date.now(),
+        text: msg,
+        username: "Toxic#2381"
+      }
+      comments = [...comments, message];
+      event.target.text.value = "";
+    }
+  }
+
+</script>
+
 <style>
   .Comments h3 {
     font-size: 14px;
@@ -53,12 +72,17 @@
 
 <div class="Comments">
   <div class="Comments-content">
-    <div class="Comments-users">
-      <h3>Kakaroto</h3>
-      <span>Hola Toxic#2381</span>
-    </div>
+
+
+    {#each comments as comment (comment.id)}
+      <div class="Comments-users">
+        <h3>{comment.username}</h3>
+        <span>{comment.text}</span>
+      </div>
+    {/each}
+
     <div class="Comments-add">
-      <form>
+      <form on:submit|preventDefault={addComment}>
         <input type="text" 
         class="Comments-input" 
         placeholder="Agregar Comentario..."
